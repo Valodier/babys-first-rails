473d268016f4710ce01344f5ed27df8577cef869
William Meyer <williamjmiii@gmail.com>
2022-03-09 09:26:32 -0500

    Added about_method route and about_method to welcomes_controller

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -2,5 +2,10 @@ class WelcomesController < ApplicationController
   def hello_method
     render json: {message: "Hello from the welcomes controller!"}    
   end
+
+  def about_method
+    render json: {message: "Ruby is my favorite language! Because its the only one I know right now."}
+  end
+
 end
 
